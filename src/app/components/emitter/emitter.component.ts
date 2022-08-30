9d156ef926c5358975f65d05a1fd640c2e445f99
jonas-borf <jonasbarfield@gmail.com>
2022-08-30 11:40:13 -0400

    update

---
+++ src/app/components/emitter/emitter.component.ts
@@ -0,0 +1,29 @@
+import { Component, EventEmitter, HostListener, OnInit, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-emitter',
+  templateUrl: './emitter.component.html',
+  styleUrls: ['./emitter.component.css'],
+})
+export class EmitterComponent implements OnInit {
+  @Output() notifyTimerStart = new EventEmitter();
+  @Output() notifyGetItems = new EventEmitter();
+  @Output() notifyKeyDown = new EventEmitter();
+  @Output() notifyKeyUp = new EventEmitter();
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  @HostListener('window:keydown', ['$event'])
+  spaceEvent(event: any) {
+    console.log(event.key);
+    // event is sent here but all values are undefined for some reason
+
+    this.notifyKeyDown.emit({event: event});
+  }
+   @HostListener('window:keyup', ['$event'])
+  keyEvent(event: KeyboardEvent) {
+    this.notifyKeyUp.emit({event: event});
+  }
+  // keep all the event emmitters here for better organization
+}
